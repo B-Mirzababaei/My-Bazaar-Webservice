f7b151db8d77c5ae36fde608f648d51af7a618f8
Behzad Mirzababaei <bmirzababaei@know-center.at>
2020-06-24 20:40:14 +0200

    change address to local

--- bazaar/public/scripts/config.js
+++ bazaar/public/scripts/config.js
@@ -1,3 +1,3 @@
 // Config Variable
-// const hostURL = 'http://127.0.0.1';
-const config_hostURL = "http://rebo4ai.know-center.tugraz.at";
+const config_hostURL = 'http://127.0.0.1';
+//const config_hostURL = "http://rebo4ai.know-center.tugraz.at";
